b8522990ab650d85177a2291d53cba2f5270c3a4
Fumitaka Nozawa <fumitaka05@gmail.com>
2015-01-30 18:30:11 +0900

    img追加

---
+++ fizzbuzz/lib/fizzbuzz.rb
@@ -0,0 +1,14 @@
+class Fizzbuzz
+  def calculate number
+    if number % 3 == 0 && number % 5 == 0
+      'fizzbuzz'
+    elsif number % 3 == 0
+      'fizz'
+    elsif number % 5 == 0
+      'buzz'
+    elsif number.to_s.include? '7'
+      'GitHub'
+      number
+    end
+  end
+end
